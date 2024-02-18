f773306828f3892132282bf58fe3c6a21a017ee4
castilloglenn <castilloglenn@ymail.com>
2024-02-18 23:28:34 +0800

    feat: color palette

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -3,6 +3,7 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
+// Palette: https://coolors.co/145c9e-ffbe0b-db162f-121212-eff1f3
 export default new Vuetify({
   theme: { dark: true },
 });
